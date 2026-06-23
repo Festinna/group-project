8057eff187515e39ea5f1bd5ae14bd199dac128d
Festinna <festinakrasniqi09@gmail.com>
2026-06-23 17:52:16 -0600

    f

--- anisafestina/about.js
+++ anisafestina/about.js
@@ -1,33 +1,18 @@
 const menu = document.getElementById("menu");
-
 const nav = document.getElementById("navLinks");
 
-
 menu.addEventListener("click",()=>{
-
 nav.classList.toggle("show");
-
 });
 
-
-
 window.addEventListener("scroll",()=>{
-
-
 let navbar=document.querySelector(".navbar");
-
-
 if(window.scrollY>50){
-
 navbar.style.boxShadow="0 5px 25px #ff1744";
-
 }
 
 else{
-
 navbar.style.boxShadow="none";
-
 }
-
 
 });
